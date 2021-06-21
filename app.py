f1f1c545793a595f338b224bcabe2ad1febb5caf
yashgoel236437 <yashgoel236437@gmail.com>
2021-06-21 13:15:08 +0530

    trial4

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ app = Flask(__name__)
 def api():
     a=request.get_json()
     
-    delhi = joblib.load("delhi.pb")
+    #delhi = joblib.load("delhi.pb")
     #mumbai = joblib.load("mumbai.pb")
     #bangalore = joblib.load("bangalore.pb")
     #chennai = joblib.load("chennai.pb")
     #kolkata = joblib.load("kolkata.pb")
     #hyderabad = joblib.load("hyderabad.pb")
     
+    city=a['City']
+    a=a['Prediction']
+    a=np.array(a)
+    a=np.expand_dims(a,0)
     
+    
+    #result={"price":prediction}
     
     return "hello world"
     
